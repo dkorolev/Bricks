f59dcb31d6cc9739cfbd221a20748fce9cea075c
Dima Korolev <dmitry.korolev@gmail.com>
2019-03-10 22:49:19 -0700

    Branched out `base.h`.

--- optimize/vars/vars.h
+++ optimize/vars/vars.h
@@ -30,7 +30,7 @@
 #include <string>
 #include <vector>
 
-#include "../../bricks/exception.h"
+#include "../base.h"
 
 #include "../../typesystem/serialization/json.h"
 #include "../../typesystem/struct.h"
@@ -40,8 +40,8 @@
 namespace current {
 namespace expression {
 
-struct VarsManagementException final : current::Exception {
-  using Exception::Exception;
+struct VarsManagementException final : OptimizeException {
+  using OptimizeException::OptimizeException;
 };
 
 // When the "weakly typed" tree of possibly multidimensional var nodes is used in the way other than it was initialized.
@@ -50,21 +50,21 @@ struct VarsManagementException final : current::Exception {
 //   c["foo"][0] throws.
 //   c["foo"]["bar"]["baz"] throws.
 //   c[42] throws.
-struct VarNodeTypeMismatchException final : current::Exception {};
+struct VarNodeTypeMismatchException final : OptimizeException {};
 
 // When the "internal leaf allocation index" is requested for the var path that is not a leaf.
 // I.e.:
 //   c["test"]["passed"] = 42;
 //   c["whatever"] = 0;
 //   c["test"] + c["whatever"] throws, because `["test"]` is a node, not a leaf, in the vars tree of `c[]`.
-struct VarIsNotLeafException final : current::Exception {};
+struct VarIsNotLeafException final : OptimizeException {};
 
 // When the value is attempted to be re-assigned. I.e.: `c["foo"] = 1; c["foo"] = 2;`.
-struct VarNodeReassignmentAttemptException final : current::Exception {};
+struct VarNodeReassignmentAttemptException final : OptimizeException {};
 
 // When the variables tree is attempted to be changed after being `.Finalize()`-s.
 // I.e., `c["foo"] = 1.0; vars_context.Finalize(); c["bar"] = 2.0;`.
-struct VarsTreeFinalizedException final : current::Exception {};
+struct VarsTreeFinalizedException final : OptimizeException {};
 
 class VarsContextInterface {
  public:
@@ -85,13 +85,13 @@ class VarsManager final {
   static VarsManager& TLS() { return ThreadLocalSingleton<VarsManager>(); }
   VarsContext& Active() const {
     if (!active_context_) {
-      CURRENT_THROW(VarsManagementException("Vars context is required."));
+      CURRENT_THROW(VarsManagementException("The variables context is required."));
     }
     return *active_context_;
   }
   void SetActive(VarsContext* ptr1, VarsContextInterface* ptr2) {
     if (active_context_) {
-      CURRENT_THROW(VarsManagementException("Attempted to create nested context for variables."));
+      CURRENT_THROW(VarsManagementException("Attempted to create a nested variables context."));
     }
     active_context_ = ptr1;
     active_context_interface_ = ptr2;
@@ -135,10 +135,9 @@ CURRENT_STRUCT(I) { CURRENT_FIELD(z, (std::map<uint32_t, Node>)); };
 CURRENT_STRUCT(S) { CURRENT_FIELD(z, (std::map<std::string, Node>)); };
 }  // namespace current::expression::json
 
+enum class VarNodeType { Unset, Vector, IntMap, StringMap, Value };
 struct VarNode {
-  enum class Type { Unset, Vector, IntMap, StringMap, Value };
-
-  Type type = Type::Unset;
+  VarNodeType type = VarNodeType::Unset;
   std::vector<VarNode> children_vector;                  // `type == Vector`.
   std::map<size_t, VarNode> children_int_map;            // `type == IntMap`.
   std::map<std::string, VarNode> children_string_map;    // `type == StringMap`.
@@ -155,19 +154,19 @@ struct VarNode {
       //                 this optimization code should be used to optimize an 1M++-variables model.
       CURRENT_THROW(VarsManagementException("Attempted to create a dense vector of the wrong size."));
     }
-    if (type == Type::Unset) {
-      type = Type::Vector;
+    if (type == VarNodeType::Unset) {
+      type = VarNodeType::Vector;
       children_vector.resize(dim);
-    } else if (!(type == Type::Vector && children_vector.size() == dim)) {
+    } else if (!(type == VarNodeType::Vector && children_vector.size() == dim)) {
       CURRENT_THROW(VarNodeTypeMismatchException());
     }
   }
 
   VarNode& operator[](size_t i) {
     if (VarsManager::TLS().ActiveViaInterface().IsFinalized()) {
-      if (type == Type::Vector && i < children_vector.size()) {
+      if (type == VarNodeType::Vector && i < children_vector.size()) {
         return children_vector[i];
-      } else if (type == Type::IntMap) {
+      } else if (type == VarNodeType::IntMap) {
         auto const cit = children_int_map.find(i);
         if (cit != children_int_map.end()) {
           return cit->second;
@@ -175,17 +174,17 @@ struct VarNode {
       }
       CURRENT_THROW(VarsTreeFinalizedException());
     }
-    if (type == Type::Vector) {
+    if (type == VarNodeType::Vector) {
       if (i < children_vector.size()) {
         return children_vector[i];
       } else {
-        CURRENT_THROW(VarsManagementException("Out of bounds for dense variables node."));
+        CURRENT_THROW(VarsManagementException("Out of bounds for the dense variables node."));
       }
     }
-    if (type == Type::Unset) {
-      type = Type::IntMap;
+    if (type == VarNodeType::Unset) {
+      type = VarNodeType::IntMap;
     }
-    if (type != Type::IntMap) {
+    if (type != VarNodeType::IntMap) {
       CURRENT_THROW(VarNodeTypeMismatchException());
     }
     return children_int_map[i];
@@ -193,7 +192,7 @@ struct VarNode {
 
   VarNode& operator[](std::string const& s) {
     if (VarsManager::TLS().ActiveViaInterface().IsFinalized()) {
-      if (type == Type::StringMap) {
+      if (type == VarNodeType::StringMap) {
         auto const cit = children_string_map.find(s);
         if (cit != children_string_map.end()) {
           return cit->second;
@@ -201,10 +200,10 @@ struct VarNode {
       }
       CURRENT_THROW(VarsTreeFinalizedException());
     }
-    if (type == Type::Unset) {
-      type = Type::StringMap;
+    if (type == VarNodeType::Unset) {
+      type = VarNodeType::StringMap;
     }
-    if (type != Type::StringMap) {
+    if (type != VarNodeType::StringMap) {
       CURRENT_THROW(VarNodeTypeMismatchException());
     }
     return children_string_map[s];
@@ -214,8 +213,8 @@ struct VarNode {
     if (VarsManager::TLS().ActiveViaInterface().IsFinalized()) {
       CURRENT_THROW(VarsTreeFinalizedException());
     }
-    if (type != Type::Unset) {
-      if (type == Type::Value) {
+    if (type != VarNodeType::Unset) {
+      if (type == VarNodeType::Value) {
         if (value == x) {
           return;  // A valid no-op. -- D.K.
         } else {
@@ -225,62 +224,62 @@ struct VarNode {
         CURRENT_THROW(VarNodeTypeMismatchException());
       }
     }
-    type = Type::Value;
+    type = VarNodeType::Value;
     value = x;
     internal_leaf_index = VarsManager::TLS().ActiveViaInterface().AllocateVar();
   }
 
   size_t InternalLeafIndex() const {
-    if (type != Type::Value) {
+    if (type != VarNodeType::Value) {
       CURRENT_THROW(VarIsNotLeafException());
     }
     return internal_leaf_index;
   }
 
   void DFSToStampFinalizedIndexes(uint32_t& next_finalized_index) {
-    if (type == Type::Vector) {
+    if (type == VarNodeType::Vector) {
       for (VarNode& c : children_vector) {
         c.DFSToStampFinalizedIndexes(next_finalized_index);
       }
-    } else if (type == Type::IntMap) {
+    } else if (type == VarNodeType::IntMap) {
       for (auto& e : children_int_map) {
         e.second.DFSToStampFinalizedIndexes(next_finalized_index);
       }
-    } else if (type == Type::StringMap) {
+    } else if (type == VarNodeType::StringMap) {
       for (auto& e : children_string_map) {
         e.second.DFSToStampFinalizedIndexes(next_finalized_index);
       }
-    } else if (type == Type::Value) {
+    } else if (type == VarNodeType::Value) {
       finalized_index = next_finalized_index;
       ++next_finalized_index;
-    } else if (type != Type::Unset) {
+    } else if (type != VarNodeType::Unset) {
       CURRENT_THROW(VarsManagementException("Attempted to `DFSToStampFinalizedIndexes()` on an invalid var node."));
     }
   }
 
   json::Node DoDump() const {
-    if (type == Type::Vector) {
+    if (type == VarNodeType::Vector) {
       json::V dense;
       dense.z.reserve(children_vector.size());
       for (VarNode const& c : children_vector) {
         dense.z.push_back(c.DoDump());
       }
       return dense;
-    } else if (type == Type::IntMap) {
+    } else if (type == VarNodeType::IntMap) {
       json::I sparse_by_int;
       for (std::pair<size_t, VarNode> const& e : children_int_map) {
         sparse_by_int.z[static_cast<uint32_t>(e.first)] = e.second.DoDump();
       }
       return sparse_by_int;
-    } else if (type == Type::StringMap) {
+    } else if (type == VarNodeType::StringMap) {
       json::S sparse_by_string;
       for (std::pair<std::string, VarNode> const& e : children_string_map) {
         sparse_by_string.z[e.first] = e.second.DoDump();
       }
       return sparse_by_string;
-    } else if (type == Type::Value) {
+    } else if (type == VarNodeType::Value) {
       return json::X(value, static_cast<uint32_t>(internal_leaf_index), finalized_index);
-    } else if (type == Type::Unset) {
+    } else if (type == VarNodeType::Unset) {
       return json::U();
     } else {
       CURRENT_THROW(VarsManagementException("Attempted to Dump() an invalid var node."));
@@ -319,7 +318,7 @@ class VarsContext final : public VarsContextInterface {
   uint32_t AllocateVar() override {
     VarsManager::TLS().ConfirmActive(this, this);
     if (finalized_) {
-      CURRENT_THROW(VarsManagementException("Attempted to `AllocateVar()` after it's `Finalize()`-ed."));
+      CURRENT_THROW(VarsManagementException("Attempted to `AllocateVar()` after the vars context is `Finalize()`-ed."));
     } else {
       return leaves_allocated_++;
     }
