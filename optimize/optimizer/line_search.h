f2273e3c2bf78530391ba6172f08cd4613cc1243
Dima Korolev <dmitry.korolev@gmail.com>
2019-03-15 08:13:22 -0700

    Unified 1D tests, plots for them, and a simple Newton's method inside.

--- optimize/optimizer/line_search.h
+++ optimize/optimizer/line_search.h
@@ -44,23 +44,94 @@ struct OptimizationException final : OptimizeException {
 
 namespace optimizer {
 
+struct LineSearchResult {
+  double best_step;
+  struct IntermediatePoint {
+    double step;
+    double f;
+    double df;
+  };
+  std::vector<IntermediatePoint> path;
+  std::vector<char const*> comments;
+};
+
 class LineSearchImpl final {
  public:
-  static double DoLineSearch(LineSearchContext const& self) {
+  static LineSearchResult DoLineSearch(LineSearchContext const& self) {
+    LineSearchResult result;
+
     // NOTE(dkorolev): IMPORTANT: To ensure all the values are valid, for each new step size the order of computations
     // should be: `l`, `d`, and then `ds`. This is to ensure the internal node values cache is kept valid all the time.
     double const value_at_0 = self.l(self.jit_call_context, self.vars_mapper.x, 0.0);
-    static_cast<void>(value_at_0);
     double const derivative_at_0 = self.d(self.jit_call_context, self.vars_mapper.x, 0.0);
+
+    result.path.push_back(LineSearchResult::IntermediatePoint{0.0, value_at_0, derivative_at_0});
+
     if (derivative_at_0 < 0) {
       CURRENT_THROW(OptimizationException("Line search should begin in the direction of the gradient."));
     }
     double const second_derivative_at_0 = (*self.more_ds[0])(self.jit_call_context, self.vars_mapper.x, 0.0);
-    return -(derivative_at_0 / second_derivative_at_0);
+    // TODO(dkorolev): Need to look at the sign of the second derivative, and to compare it to zero.
+
+    double const step = -(derivative_at_0 / second_derivative_at_0);
+    double const value_at_step = self.l(self.jit_call_context, self.vars_mapper.x, step);
+    double const derivative_at_step = self.d(self.jit_call_context, self.vars_mapper.x, step);
+
+    result.path.push_back(LineSearchResult::IntermediatePoint{step, value_at_step, derivative_at_step});
+
+    // TODO(dkorolev): No bare `1e-6` here, introduce optimization parameters.
+    if (fabs(derivative_at_step) < 1e-6) {
+      result.comments.push_back("bingo");
+      result.best_step = step;
+      return result;
+    } else if (derivative_at_step < 0) {
+      result.comments.push_back("overshot");
+      double a = 0;
+      double va = derivative_at_0;
+      double b = step;
+      double vb = derivative_at_step;
+
+      double c;
+
+      for (size_t iteration = 0; iteration < 10; ++iteration) {
+        double const k = va / (va - vb);
+        if (!(k > 0 && k < 1)) {
+          CURRENT_THROW(OptimizationException("The Newton method is failing, same signs at the ends, internal error."));
+        }
+        c = a + (b - a) * k;
+        double const value_at_c = self.l(self.jit_call_context, self.vars_mapper.x, c);
+        double const derivative_at_c = self.d(self.jit_call_context, self.vars_mapper.x, c);
+
+        result.path.push_back(LineSearchResult::IntermediatePoint{c, value_at_c, derivative_at_c});
+
+        if (fabs(derivative_at_c) < 1e-6) {
+          result.comments.push_back("bingo");
+          result.best_step = c;
+          return result;
+        } else {
+          result.comments.push_back("newton");
+          if (derivative_at_step < 0) {
+            a = c;
+            va = derivative_at_c;
+          } else {
+            b = c;
+            vb = derivative_at_c;
+          }
+        }
+      }
+
+      result.comments.push_back("well, we tried");
+      result.best_step = c;
+      return result;
+    } else {
+      result.comments.push_back("not implemented yet");
+      result.best_step = step;
+      return result;
+    }
   }
 };
 
-inline double LineSearch(LineSearchContext const& self) { return LineSearchImpl::DoLineSearch(self); }
+inline LineSearchResult LineSearch(LineSearchContext const& self) { return LineSearchImpl::DoLineSearch(self); }
 
 }  // namespace current::expression::optimizer
 }  // namespace current::expression
--- optimize/optimizer/test.cc
+++ optimize/optimizer/test.cc
@@ -24,9 +24,13 @@ SOFTWARE.
 
 #include "line_search.h"
 
-#include "../../3rdparty/gtest/gtest-main.h"
+#include "../../bricks/graph/gnuplot.h"
+
+#include "../../3rdparty/gtest/gtest-main-with-dflags.h"
 
 #ifdef FNCAS_X64_NATIVE_JIT_ENABLED
+
+DEFINE_bool(save_line_search_test_plots, false, "Set to have each 1D optimization regression test to be plotted.");
 
 TEST(OptimizationOptimizerLineSearch, QuadraticFunction) {
   using namespace current::expression;
@@ -50,7 +54,7 @@ TEST(OptimizationOptimizerLineSearch, QuadraticFunction) {
   optimization_context.compiled_g(optimization_context.jit_call_context, optimization_context.vars_mapper.x);
 
   // In case of the quadratic function, see `../differentiate/test.cc`, the first and best step is always  `-0.5`.
-  EXPECT_EQ(-0.5, LineSearch(line_search_context));
+  EXPECT_EQ(-0.5, LineSearch(line_search_context).best_step);
 
   // This step should take the function to its optimum, which, in this case, is the minimum, equals to zero.
   EXPECT_EQ(
@@ -63,5 +67,98 @@ TEST(OptimizationOptimizerLineSearch, QuadraticFunction) {
   EXPECT_EQ("[3.0]", JSON(optimization_context.CurrentPoint()));
   EXPECT_EQ(0.0, optimization_context.ComputeCurrentObjectiveFunctionValue());
 }
+
+inline void SavePlotAndLineSearchPath(std::string const& test_name,
+                                      std::string const& function_as_string,
+                                      current::expression::optimizer::OptimizationContext const& optimization_context,
+                                      current::expression::optimizer::LineSearchResult const& result,
+                                      double derivative_value) {
+  using namespace current::gnuplot;
+
+#ifndef CURRENT_APPLE
+  const char* const format = "pngcairo";
+#else
+  const char* const format = "png";
+#endif
+  const char* const extension = "png";
+
+  const std::string plot_body =
+      GNUPlot()
+          .Title("f(x) = " + function_as_string)
+          .Grid("back")
+          .XLabel("x")
+          .YLabel("f(x), f'(x), path")
+          .Plot(WithMeta([&optimization_context](Plotter p) {
+                  for (int i = -50; i <= +1050; ++i) {
+                    double const x = 0.01 * i;
+                    double const y = optimization_context.compiled_f(optimization_context.jit_call_context, {x});
+                    p(x, y);
+                  }
+                })
+                    .Name("f(x)")
+                    .LineWidth(10)
+                    .Color("rgb '#D0FFD0'"))
+          .Plot(WithMeta([&optimization_context](Plotter p) {
+                  for (int i = -50; i <= +1050; ++i) {
+                    double const x = 0.01 * i;
+                    double const unused_but_MUST_be_computed =
+                        optimization_context.compiled_f(optimization_context.jit_call_context, {x});
+                    static_cast<void>(unused_but_MUST_be_computed);
+                    double const y = optimization_context.compiled_g(optimization_context.jit_call_context, {x})[0];
+                    p(x, y);
+                  }
+                })
+                    .Name("f'(x)")
+                    .LineWidth(5)
+                    .Color("rgb '#0000FF'"))
+          .Plot(WithMeta([&result, derivative_value](Plotter p) {
+                  for (current::expression::optimizer::LineSearchResult::IntermediatePoint const& pt : result.path) {
+                    p(pt.step * derivative_value, pt.f);
+                  }
+                })
+                    .Name("path")
+                    .LineWidth(2)
+                    .Color("rgb '#FF0000'"))
+          .ImageSize(800)
+          .OutputFormat(format);
+
+  // OK to use the non-portable `.current/` here, as this code is a) flag-protected, and b) System V only. -- D.K.
+  current::FileSystem::WriteStringToFile(plot_body, (".current/" + test_name + '.' + extension).c_str());
+}
+
+#define TEST_1D_LINE_SEARCH(test_name, function_body, expected_final_value, expected_comments)                         \
+  TEST(OptimizationOptimizerLineSearch, RegressionTest##test_name) {                                                   \
+    using namespace current::expression;                                                                               \
+    VarsContext vars_context;                                                                                          \
+    x[0] = 0.0;                                                                                                        \
+    vars_context.ReindexVars();                                                                                        \
+    value_t const f = [](value_t x) { return function_body; }(x[0]);                                                   \
+    using namespace current::expression::optimizer;                                                                    \
+    OptimizationContext optimization_context(vars_context, f);                                                         \
+    LineSearchContext const line_search_context(optimization_context);                                                 \
+    optimization_context.compiled_f(optimization_context.jit_call_context, optimization_context.vars_mapper.x);        \
+    double const derivative_value =                                                                                    \
+        optimization_context.compiled_g(optimization_context.jit_call_context, optimization_context.vars_mapper.x)[0]; \
+    LineSearchResult const result = LineSearch(line_search_context);                                                   \
+    double const step_size = result.best_step;                                                                         \
+    optimization_context.MovePointAlongGradient(step_size);                                                            \
+    double const final_value = optimization_context.ComputeCurrentObjectiveFunctionValue();                            \
+    if (FLAGS_save_line_search_test_plots) {                                                                           \
+      SavePlotAndLineSearchPath(#test_name, #function_body, optimization_context, result, derivative_value);           \
+    }                                                                                                                  \
+    EXPECT_EQ(expected_comments, current::strings::Join(result.comments, "; "));                                       \
+    EXPECT_NEAR(expected_final_value, final_value, 1e-6);                                                              \
+  }
+
+// This is a simple order-two function, with a clearly visible minumum at `x = 6`, found in a single Newtop step.
+TEST_1D_LINE_SEARCH(00Parabola, 5 + sqr(x - 6), 5.0, "bingo")
+
+// A modification to the above test to make it an order-three function, so that the first step "overshoots" `x = 6`.
+TEST_1D_LINE_SEARCH(01SlightlyCubicParabola,
+                    5 + (x - 6) * (x - 6) * (1 + 0.03 * (x - 6)),
+                    5.0,
+                    "overshot; newton; newton; newton; newton; newton; newton; newton; newton; newton; bingo")
+
+#undef TEST_1D_LINE_SEARCH
 
 #endif  // FNCAS_X64_NATIVE_JIT_ENABLED
