ae37aa25ddf77df14332d3ca2cdbc6ad92f29a7f
Dima Korolev <dmitry.korolev@gmail.com>
2019-03-16 10:49:12 -0700

    Basic optimization and a smoke test for it.

--- optimize/optimizer/optimizer.h
+++ optimize/optimizer/optimizer.h
@@ -25,8 +25,10 @@ SOFTWARE.
 #ifndef OPTIMIZE_OPTIMIZER_OPTIMIZER_H
 #define OPTIMIZE_OPTIMIZER_OPTIMIZER_H
 
-#include "optimizer_base.h"
 #include "context.h"
+#include "optimizer_base.h"
+
+#include "line_search/line_search.h"
 
 #ifdef FNCAS_X64_NATIVE_JIT_ENABLED
 
@@ -34,7 +36,57 @@ namespace current {
 namespace expression {
 namespace optimizer {
 
-inline int Foo() { return 42; }
+struct OptimizationResult {
+  // The final result.
+  double final_value;
+  std::vector<double> final_point;
+
+  // Some rudimentary history.
+  std::vector<std::vector<double>> trace;
+  std::vector<double> values;
+  std::vector<double> steps;
+};
+
+inline OptimizationResult Optimize(OptimizationContext& optimization_context) {
+  size_t const kMaxIterations = 10;
+  double const kMinImprovementPerIteration = 1e-6;
+  double const kMinStep = 1e-6;
+
+  OptimizationResult result;
+
+  LineSearchContext const line_search_context(optimization_context);
+
+  double const starting_value =
+      optimization_context.compiled_f(optimization_context.jit_call_context, optimization_context.vars_mapper.x);
+
+  result.values.push_back(starting_value);
+  result.trace.push_back(optimization_context.vars_mapper.x);
+
+  for (size_t iteration = 0; iteration < kMaxIterations; ++iteration) {
+    optimization_context.compiled_g(optimization_context.jit_call_context, optimization_context.vars_mapper.x);
+
+    double const step = LineSearch(line_search_context).best_step;
+    if (-step < kMinStep) {
+      break;
+    }
+    optimization_context.MovePointAlongGradient(step);
+
+    result.steps.push_back(step);
+    result.trace.push_back(optimization_context.vars_mapper.x);
+
+    result.values.push_back(
+        optimization_context.compiled_f(optimization_context.jit_call_context, optimization_context.vars_mapper.x));
+
+    if (result.values.size() >= 2 &&
+        ((*(result.values.rbegin() + 1) - result.values.back()) < kMinImprovementPerIteration)) {
+      break;
+    }
+  }
+
+  result.final_point = result.trace.back();
+
+  return result;
+}
 
 }  // namespace current::expression::optimizer
 }  // namespace current::expression
--- optimize/optimizer/test.cc
+++ optimize/optimizer/test.cc
@@ -26,7 +26,61 @@ SOFTWARE.
 
 #include "../../3rdparty/gtest/gtest-main-with-dflags.h"
 
-TEST(OptimizationOptimizer, Foo) {
+TEST(OptimizationOptimizer, TrivialSingleStepOptimization) {
+  using namespace current::expression;
   using namespace current::expression::optimizer;
-  EXPECT_EQ(42, Foo());
+
+  VarsContext vars_context;
+
+  x[0] = 0;
+  x[1] = 0;
+
+  value_t const f = sqr(x[0] - 3) + sqr(x[1] - 5);
+
+  OptimizationContext optimization_context(vars_context, f);
+
+  OptimizationResult const result = Optimize(optimization_context);
+
+  EXPECT_EQ("[34.0,0.0]", JSON(result.values));
+  EXPECT_EQ("[[0.0,0.0],[3.0,5.0]]", JSON(result.trace));
+  EXPECT_EQ("[-0.5]", JSON(result.steps));
+
+  // A single integer step, the result is exact.
+  EXPECT_EQ("[3.0,5.0]", JSON(result.final_point));
+  ASSERT_EQ(2u, result.final_point.size());
+  EXPECT_EQ(3, result.final_point[0]);
+  EXPECT_EQ(5, result.final_point[1]);
+  EXPECT_EQ(0.0, result.final_value);
+
+  EXPECT_EQ(0.0, optimization_context.compiled_f(optimization_context.jit_call_context, result.final_point));
+  EXPECT_EQ("[0.0,0.0]",
+            JSON(optimization_context.compiled_g(optimization_context.jit_call_context, result.final_point)));
+}
+
+TEST(OptimizationOptimizer, MultiStepOptimization) {
+  using namespace current::expression;
+  using namespace current::expression::optimizer;
+
+  VarsContext vars_context;
+
+  x[0] = 0.0;
+  x[1] = 0.0;
+
+  value_t const f = log(1 + exp(x[0] - 3)) + log(1 + exp(3 - x[0])) + log(1 + exp(x[1] - 5)) + log(1 + exp(5 - x[1]));
+
+  OptimizationContext optimization_context(vars_context, f);
+  OptimizationResult const result = Optimize(optimization_context);
+
+  EXPECT_EQ(
+      "["
+      "[0.0,0.0],"
+      "[3.8657811794817156,4.213713024184646],"
+      "[3.0039879355304568,5.0043468627153129],"
+      "[2.999999999460174,5.000000000495253],"
+      "[3.0,5.0]"
+      "]",
+      JSON(result.trace));
+  EXPECT_EQ("[8.11060540012572,3.105144018869468,2.7725974219447426,2.772588722239781,2.772588722239781]",
+            JSON(result.values));
+  EXPECT_EQ("[-4.270881774245182,-2.11361538852622,-2.0000029213342627,-1.9999998287475738]", JSON(result.steps));
 }
