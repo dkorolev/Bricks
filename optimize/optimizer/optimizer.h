cfa8a7ecdbee1c05d4356c786562978c90964b17
Dima Korolev <dmitry.korolev@gmail.com>
2019-04-16 17:05:50 -0700

    Making moving the point along the gradient customizable.

--- optimize/optimizer/optimizer.h
+++ optimize/optimizer/optimizer.h
@@ -53,22 +53,71 @@ CURRENT_STRUCT(OptimizationParameters) {
   CURRENT_FIELD(min_improvement_per_iteration, double, 1e-10);
   CURRENT_FIELD(min_improvement_per_two_iterations, double, 1e-9);
   CURRENT_FIELD(min_step, double, 1e-9);
+  CURRENT_FIELD(line_search_parameters, LineSearchParameters);
 };
 
+class GradientAccessor {
+ private:
+  std::vector<double> const& vars_values_;
+  double const* ram_;
+  std::vector<value_t> const& g_;
+
+ public:
+  explicit GradientAccessor(OptimizationContext const& optimization_context)
+      : vars_values_(optimization_context.vars_values.x),
+        ram_(optimization_context.jit_call_context.ConstRAMPointer()),
+        g_(optimization_context.g) {}
+
+  size_t size() const { return g_.size(); }
+
+  double operator[](size_t i) const {
+    return ExpressionNodeIndexFromExpressionNodeOrValue(g_[i]).CheckedDispatch<double>(
+        [&](size_t node_index) { return ram_[node_index]; },
+        [&](size_t var_index) { return vars_values_[var_index]; },
+        [&](double x) { return x; },
+        []() -> double { CURRENT_THROW(VarsMapperMovePointUnexpectedLambdaException()); });
+  }
+};
 class OptimizationStrategy {
  private:
   OptimizationParameters const parameters_;
 
  public:
   explicit OptimizationStrategy(OptimizationParameters parameters) : parameters_(std::move(parameters)) {}
+
   OptimizationParameters const& Parameters() const { return parameters_; }
+  LineSearchParameters const& LineSearchParameters() const { return parameters_.line_search_parameters; }
+
+  bool StopByStepSize(uint32_t one_based_iteration_index, double step_size) const {
+    static_cast<void>(one_based_iteration_index);
+    return step_size < parameters_.min_step;
+  }
+
+  bool StopByNoImprovement(OptimizationResult const& result) const {
+    std::vector<double> const& values = result.values;
+    if (values.size() >= 2 && ((*(values.rbegin() + 1) - values.back()) < parameters_.min_improvement_per_iteration)) {
+      return true;
+    }
+    if (values.size() >= 3 &&
+        ((*(values.rbegin() + 2) - values.back()) < parameters_.min_improvement_per_two_iterations)) {
+      return true;
+    }
+    return false;
+  }
+
+  bool StopByMaxIterations(OptimizationResult const& result) const {
+    return result.iterations >= parameters_.max_iterations;
+  }
+
+  // NOTE(dkorolev): `step` is negative here.
+  void MovePointAlongGradient(std::vector<double>& x, GradientAccessor const& dx, double step) const {
+    for (size_t i = 0; i < x.size(); ++i) {
+      x[i] += dx[i] * step;
+    }
+  }
 };
 
 inline OptimizationResult Optimize(OptimizationContext& optimization_context, OptimizationStrategy const& strategy) {
-  OptimizationParameters const& parameters = strategy.Parameters();
-
-  LineSearchParameters line_search_parameters;
-
   OptimizationResult result;
 
   LineSearchContext const line_search_context(optimization_context);
@@ -83,29 +132,29 @@ inline OptimizationResult Optimize(OptimizationContext& optimization_context, Op
   do {
     optimization_context.compiled_g(optimization_context.vars_values.x);
 
-    step = LineSearch(line_search_context, line_search_parameters, step).best_step;
-    if (-Value(step) < parameters.min_step) {
+    // `Optional<double>` is provided to `LineSearch` as the best step on the previous iteration.
+    // Line search is stateful this way.
+    step = LineSearch(line_search_context, strategy.LineSearchParameters(), step).best_step;
+    double const step_value = Value(step);
+    if (strategy.StopByStepSize(static_cast<uint32_t>(result.iterations), -step_value)) {
       break;
     }
 
     ++result.iterations;
 
-    optimization_context.MovePointAlongGradient(Value(step));
+    std::vector<double> new_value(optimization_context.vars_values.x);
+    strategy.MovePointAlongGradient(new_value, GradientAccessor(optimization_context), step_value);
+    optimization_context.vars_values.InjectPoint(new_value);
 
     result.steps.push_back(Value(step));
     result.trace.push_back(optimization_context.vars_values.x);
 
     result.values.push_back(optimization_context.compiled_f(optimization_context.vars_values.x));
 
-    if (result.values.size() >= 2 &&
-        ((*(result.values.rbegin() + 1) - result.values.back()) < parameters.min_improvement_per_iteration)) {
+    if (strategy.StopByNoImprovement(result)) {
       break;
     }
-    if (result.values.size() >= 3 &&
-        ((*(result.values.rbegin() + 2) - result.values.back()) < parameters.min_improvement_per_two_iterations)) {
-      break;
-    }
-  } while (result.iterations < parameters.max_iterations);
+  } while (!strategy.StopByMaxIterations(result));
 
   result.final_value = result.values.back();
   result.final_point = result.trace.back();
